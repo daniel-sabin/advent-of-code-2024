ea22a6eff4d71f9c4ea84c1569197d3561e4191b
Daniel SABIN <sabin.daniel@gmail.com>
2024-12-19 16:18:36 +0100

    my commit to inspect

--- utils/utils.go
+++ utils/utils.go
@@ -18,7 +18,7 @@ func MapTwoSlices(a, b []int, fn func(int, int) int) []int {
 	return result
 }
 
-func Reduce(initial int, slice []int, fn func(int, int) int) int {
+func reduce(initial int, slice []int, fn func(int, int) int) int {
 	result := initial
 	for _, value := range slice {
 		result = fn(result, value)
